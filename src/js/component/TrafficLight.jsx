ad2f6d3f5b6944a5c8a0a580b9413b1ec4730e16
gomeseric <gomesericson.a@gmail.com>
2024-07-09 00:33:06 +0000

    test

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-export function TrafficLight(initialColor) {
+export function TrafficLight({initialColor}) {
     const [color, setColor] = useState(initialColor);
 
     const handleClickRed = () => {
@@ -17,28 +17,28 @@ export function TrafficLight(initialColor) {
 
     return (
         <div className="flex w-full h-screen items-center justify-center bg-red-50">
-<div className="absolute flex flex-col items-center gap-4 bg-black p-4">
+<div className="flex flex-col items-center gap-4 bg-black p-4">
   <div 
    onClick={handleClickRed}  
-   className={"h-20 w-20 rounded-full" + color === "red"
-  ? "bg-red-400"
-  : "bg-red-900"
+   className={
+    "h-20 w-20 rounded-full " + 
+    (color === "red" ? "bg-red-500" : "bg-red-900")
   }
   ></div>
   <div 
   onClick={handleClickYellow}  
-  className={"h-20 w-20 rounded-full" + color === "yellow"
-  ? "bg-yellow-400"
-  : "bg-yellow-900"
+  className={
+    "h-20 w-20 rounded-full " + 
+    (color === "yellow" ? "bg-yellow-500" : "bg-yellow-900")
   }
   ></div>
   <div 
   onClick={handleClickGreen}
-  className={"h-20 w-20 rounded-full" + color === "green"
-  ? "bg-green-400"
-  : "bg-green-900"
+  className={
+    "h-20 w-20 rounded-full " + 
+    (color === "green" ? "bg-green-500" : "bg-green-900")
   }
-  ></div>
+   ></div>
 </div>
 </div>
     );
